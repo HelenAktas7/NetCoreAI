5c96ef113a937899494819376cba189161328a23
HELEN AKTAŞ <439987@ogr.ktu.edu.tr>
2025-03-14 20:50:00 +0300

    Add project files.

---
+++ NetCoreAI.Project01_ApiDemo/Context/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAI.Project01_ApiDemo.Entities;
+namespace NetCoreAI.Project01_ApiDemo.Context
+{
+    public class ApiContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=HELEN\\SQLEXPRESS04;initial catalog=ApiAIDb ;integrated security=true;trustservercertificate=true");
+                 //Baglanti configurasyon islemlerimizi yaptik
+        }
+        public DbSet<Customer> Customers { get; set; } //Veritabanina yansitacagamiz tablo
+
+    }
+}
